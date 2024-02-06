a4fc6cad69dd2199a192bd64421a9d0eacd03201
mayabyle <mayab@moveo.com>
2024-02-06 12:21:13 +0200

    Created a table component to extract and display chef's data from the  server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from './pages/Dashboard.tsx';
-// import { FiSettings } from 'react-icons/fi';
-// import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 function App() {
 
