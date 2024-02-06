350cf4669ac323a27b4983572ed9117bac74bfed
mayabyle <mayab@moveo.com>
2024-02-06 14:46:33 +0200

    sidebar and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Dashboard from './pages/Dashboard.tsx';
+import Sidebar from './components/Sidebar/Sidebar.tsx';
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  { path: "/", element: <Layout />,
+    children: [
+      { path: "/:routeName", element: <Dashboard /> }, 
+    ] 
+  },
+])
 
 function App() {
-
-  const router = createBrowserRouter([
-    { path: "/", element: <Dashboard /> },
-  ])
 
   return (
     <div className="App">
