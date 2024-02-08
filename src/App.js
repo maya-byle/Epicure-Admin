e11e67e392c30098c9d5d3a3f9aabc55889bf360
mayabyle <mayab@moveo.com>
2024-02-08 15:08:11 +0200

    implemented initial version of Redux base functionality. Utilized thunks to efficiently extract and manage data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Dashboard from './pages/Dashboard.tsx';
 import Sidebar from './components/Sidebar/Sidebar.tsx';
 import { ROUTES } from './resources/constants.ts';
-import store from './redux/store.ts'
 
 const Layout = () => {
   return (
@@ -22,20 +21,7 @@ const router = createBrowserRouter([
   },
 ])
 
-store.subscribe(() => {
-  console.log("Store Changed:", store.getState())
-})
-
 function App() {
-  store.dispatch({
-    type: "ADD_DATA",
-    payload: "maya"
-  })
-  store.dispatch({
-    type: "DELETE_DATA",
-    payload: {name: "maya"}
-  })
-  
   return (
     <div className="App">
       <RouterProvider router={router}/>
